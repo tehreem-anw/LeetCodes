4690b8049a1b8679dc805d3fb6ed6648a248a8cd
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-17 13:43:26 -0400

    Update 704-BinarySearch.cpp

--- Easy/704-BinarySearch.cpp
+++ Easy/704-BinarySearch.cpp
@@ -1,1 +1,13 @@
-
+//helper
+int bs(int* nums, int tar, int st, int end){
+    if(st <= end){
+        int mid = (st+end) / 2;
+        if(nums[mid] == tar){return mid;}
+        else if(nums[mid] < tar){return bs(nums, tar, mid+1, end);}
+        else{return bs(nums, tar, st, mid-1);}
+    }
+    return -1;
+}
+int search(int* nums, int numsSize, int target) {
+    return bs(nums, target, 0, numsSize - 1);
+}
