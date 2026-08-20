7a95fc99ace24b512a1c7febb78749ea47e762b0
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-20 15:03:12 -0400

    Update 90-SubsetsII.cpp

--- Medium/90-SubsetsII.cpp
+++ Medium/90-SubsetsII.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    void subsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubsets){
+        if(i == nums.size()){
+            allSubsets.push_back(ans);
+            return;
+        }
+        ans.push_back(nums[i]);
+        subsets(nums, ans, i+1, allSubsets);
+        ans.pop_back();
+        
+        int idx = i + 1;
+        while(idx < nums.size() && nums[idx] == nums[idx-1]){idx++;}
 
+        subsets(nums, ans, idx, allSubsets);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        vector<vector<int>> allSubsets;
+        subsets(nums, ans, 0, allSubsets);
+        return allSubsets;
+    }
+};
