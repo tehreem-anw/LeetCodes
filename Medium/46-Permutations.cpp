c59c2f48f7fc409fd7643a0d968ad8038c28cc8a
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-23 14:05:08 -0400

    Update 46-Permutations.cpp

--- Medium/46-Permutations.cpp
+++ Medium/46-Permutations.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    void perms(vector<int> &nums, int idx, vector<vector<int>> &ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = idx; i < nums.size(); i++){
+            swap(nums[idx], nums[i]);
+            perms(nums, idx+1, ans);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        perms(nums, 0, ans);
+        return ans;
+    }
+};
