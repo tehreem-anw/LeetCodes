2d232b475f2e264b86650faccaec019ddd3e2732
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-31 13:28:36 -0400

    Update 206-ReversedLinkedList.cpp

--- Easy/206-ReversedLinkedList.cpp
+++ Easy/206-ReversedLinkedList.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
